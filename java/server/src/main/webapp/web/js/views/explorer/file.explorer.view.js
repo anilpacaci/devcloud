5bf9ccf9c7a49a1b54261441b57dd4831034b290
Serbay Arslanhan <serbay@serbay-Inspiron-N5110.(none)>
2013-04-05 00:03:52 +0300

    New file, new file save operations are done.

--- java/server/src/main/webapp/web/js/views/explorer/file.explorer.view.js
+++ java/server/src/main/webapp/web/js/views/explorer/file.explorer.view.js
@@ -32,7 +32,7 @@ define(['jquery', 'backbone', 'marionette', 'ace', 'text!templates/explorer/file
 				fileName = file.get('fileName').split('.')[0];
 
 				if ($('#editorRegion' + fileName).size() == 0) {
-					$('#tabs').append('<li class><a href="#editorRegion' + fileName + '" data-toggle="tab"><i class="icon-check"></i> ' + file.get('fileName') + ' <i class="icon-remove"></i></a></li>');
+					$('#tabs').append('<li class><a href="#editorRegion' + fileName + '" data-toggle="tab">' + file.get('fileName') + ' <i class="icon-remove"></i></a></li>');
 					$('#tab_content').append('<div class="tab-pane fade" id="editorRegion' + fileName + '"></div>');
 
 					var editorView = new EditorView({
